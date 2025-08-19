76bcb099160955c2ae59de1ce6553b11822dcc04
V.harish00 <v.harish00@gmail.com>
2025-08-19 20:13:44 +0530

    test

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,12 @@ import { LuArrowRightFromLine } from "react-icons/lu";
 
 export default function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <h1 className="text-4xl font-bold text-center">
-        Welcome to Our Website
-        <Button variant="outline" className="ml-4">
-          Get Started <LuArrowRightFromLine />
-        </Button>
-      </h1>
+    <div>
+      <div class="flex">
+        <div class="w-14 flex-none ...">01</div>
+        <div class="w-64 flex-1 ...">02</div>
+        <div class="w-32 flex-1 ...">03</div>
+      </div>
     </div>
   );
 }

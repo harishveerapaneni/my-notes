fb44b2a2543445255dffa2954e0214c5c32b8fe5
V.harish00 <v.harish00@gmail.com>
2025-08-19 20:35:45 +0530

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,13 @@ import { LuArrowRightFromLine } from "react-icons/lu";
 export default function Home() {
   return (
     <div>
-      <div className="flex">
-        <div className="w-14 flex-none ...">01</div>
-        <div className="w-64 flex-1 ...">02</div>
-        <div className="w-32 flex-1 ...">03</div>
+      <div className="flex border gap-6 p-10  border-gray-700">
+        <div className="w-96 border rounded-2xl min-h-screen border-gray-700 flex-none ...">
+          01
+        </div>
+        <div className=" border min-h-screen rounded-2xl  border-gray-700 flex-1 ...">
+          02
+        </div>
       </div>
     </div>
   );
